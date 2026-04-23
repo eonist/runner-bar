7ed054edcd4ee5d8e9e762c6b524a256681d90bf
André J <andre@sentry.co>
2026-04-23 12:09:04 +0200

    fix: use fixedSize vertical to let popover height fit content

--- Sources/RunnerBar/PopoverView.swift
+++ Sources/RunnerBar/PopoverView.swift
@@ -109,6 +109,7 @@ struct PopoverView: View {
             .padding(.horizontal, 12)
             .padding(.vertical, 8)
         }
+        .fixedSize(horizontal: false, vertical: true)
         .frame(width: 280)
     }
 
